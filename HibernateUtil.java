dc58d9ec9312a94d6bccdec9d6ddf20d40fab683
Luciano Nascimento <luciano.nascimento@provider-it.com.br>
2017-06-27 18:39:28 -0300

    Create HibernateUtil.java

---
+++ HibernateUtil.java
@@ -0,0 +1,41 @@
+package org.pasa.sispasaint.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory;
+	private static StandardServiceRegistry registry;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				registry = new StandardServiceRegistryBuilder().configure().build();
+				MetadataSources sources = new MetadataSources(registry);
+				Metadata metadata = sources.getMetadataBuilder().build();
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+	public static SessionFactory getSession() {
+		return sessionFactory;
+	}
+}
